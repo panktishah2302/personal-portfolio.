805cbd8297d61ae1732060d6de62fa4d6bfb57c6
Pankti Shah <spankti39@gmail.com>
2024-02-18 11:02:20 +0530

    Update my website

--- script.js
+++ script.js
@@ -6,14 +6,4 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-function toggleDarkMode() {
-    const body = document.body;
-    const darkModeSwitch = document.querySelector('.dark-mode-switch i');
-
-    body.classList.toggle("dark-mode");
-    const isDarkMode = body.classList.contains("dark-mode");
-
-    // Toggle between fa-toggle-off and fa-toggle-on based on dark mode status
-    darkModeSwitch.className = isDarkMode ? 'fa-solid fa-toggle-on' : 'fa-solid fa-toggle-off';
-}
 
